9bfce25d5ff8a5c15b5a46af2842a0b34c74316c
xRegiMariPinna <110124921+xRegiMariPinna@users.noreply.github.com>
2023-06-14 14:48:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+song1 = "";
+song2 = "";
+
+song1_status = "";
+song2_status = "";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function preload() {
+	song1 = loadSound("musica1.mp3");
+	song2 = loadSound("musica2.mp3");
+}
+
+function setup() {
+	canvas = createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+	console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results) {
+	if (results.length > 0) {
+		console.log(results);
+		scoreRightWrist = results[0].pose.keypoints[10].score;
+		scoreLeftWrist = results[0].pose.keypoints[9].score;
+		
+		rightWristX = results[0].pose.rightWrist.x;
+		rightWristY = results[0].pose.rightWrist.y;
+
+		leftWristX = results[0].pose.leftWrist.x;
+		leftWristY = results[0].pose.leftWrist.y;
+		console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+	}
+}
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if (scoreRightWrist > 0.2) {
+		circle(rightWristX, rightWristY, 20);
+
+		song2.stop();
+
+		if (song1_status == false) {
+			song1.play();
+			document.getElementById("song").innerHTML = "Tocando: Lança Perfume da Rita Lee"
+		}
+	}
+
+	if (scoreLeftWrist > 0.2) {
+		circle(leftWristX, leftWristY, 20);
+
+		song1.stop();
+
+		if (song2_status == false) {
+			song2.play();
+			document.getElementById("song").innerHTML = "Tocando: Xuxuzinho da Rita Lee"
+		}
+	}
+
+}
+
+function play() {
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
+}
+
+
+
+
+
+
